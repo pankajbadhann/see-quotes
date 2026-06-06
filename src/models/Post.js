f5d431da1e0b21fcf8ceaa618f785a1937458512
pa_kAJ <pankaj4utanda@gmail.com>
2026-06-06 23:07:52 +0530

    giveStructureOnly

---
+++ src/models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Post", postSchema);
